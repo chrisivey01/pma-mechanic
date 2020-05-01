5a8b65fc500607c900377434e101fa9f6ac87249
chris <chrisivey01@gmail.com>
2020-05-01 09:04:54 -0500

    update to UI

--- src/components/ClientInformation.js
+++ src/components/ClientInformation.js
@@ -10,15 +10,20 @@ import TableRow from "@material-ui/core/TableRow";
 import TableHead from "@material-ui/core/TableHead";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
+import TablePagination from '@material-ui/core/TablePagination';
 import Button from "@material-ui/core/Button";
+import Badge from "@material-ui/core/Badge";
+import LinearProgress from "@material-ui/core/LinearProgress";
 
 import { useDispatch } from "react-redux";
 import * as apis from '../apis/apis';
 import { setClientList } from '../store/actions/index';
 
 const ClientInformation = (props) => {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  
   const dispatch = useDispatch();
-  // const [open, setOpen] = React.useState(false);
 
   const tableHeaders = [
     {
@@ -36,6 +41,11 @@ const ClientInformation = (props) => {
       dispatch(setClientList(results.data))
     });
   },[])
+
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
   
 
   const { clients } = props.data;
@@ -43,7 +53,8 @@ const ClientInformation = (props) => {
     <Card>
       <CardHeader title="Current Clients" />
       <CardContent>
-        <Table>
+        <Table
+          size='small'>
           <TableHead>
             {tableHeaders.map((row) => (
               <TableRow key={row.id}>
@@ -57,19 +68,27 @@ const ClientInformation = (props) => {
             ))}
           </TableHead>
           <TableBody>
-            {clients.map((row) => (
+            {clients.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
               <TableRow key={row.id}>
                 <TableCell align="left">{row.owner_name}</TableCell>
                 <TableCell align="left">{row.owner_number}</TableCell>
                 <TableCell align="left">{row.owner_plate}</TableCell>
-                <TableCell align="left">{row.status}</TableCell>
-                <TableCell align="left">{row.progress}</TableCell>
+                <TableCell align="left">{row.status === 0 ? <Badge color="error" badgeContent="PENDING"/> : <Badge color="primary" badgeContent="COMPLETED"/>}</TableCell>
+                <TableCell align="left"><LinearProgress variant="determinate" value={row.progress}/> </TableCell>
                 <TableCell align="left">
                   <Button>Test</Button>
                 </TableCell>
               </TableRow>
             ))}
           </TableBody>
+          <TablePagination
+            rowsPerPageOptions={[5]}
+            component="div"
+            count={clients.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onChangePage={handleChangePage}
+          />
         </Table>
       </CardContent>
     </Card>
